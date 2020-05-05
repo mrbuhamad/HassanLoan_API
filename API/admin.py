3a632b6b3d70258e5bb81058a0c37b229cf505a5
mrbuhamad <mr.buhamad@hotmail.com>
2020-05-06 01:32:32 +0300

    admin part changed

--- API/admin.py
+++ API/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from .models import Loan, Pyments, Participants,Hold
 
 
+
+class LoanInstanceInline(admin.TabularInline):
+    model = Loan
+
+class PartAdmin(admin.ModelAdmin):
+    inlines = [LoanInstanceInline]
+
+admin.site.register(Participants, PartAdmin )
 admin.site.register(Loan)
 admin.site.register(Pyments)
-admin.site.register(Participants)
 admin.site.register(Hold)
+
 
